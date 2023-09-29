134189d085600cd77771059a96096e22b11961c7
zawadul22 <zawadul22@gmail.com>
2023-09-29 17:36:56 +0600

    Updated mint page

--- src/NFTMintv2.jsx
+++ src/NFTMintv2.jsx
@@ -1,9 +1,54 @@
+import { Accordion, AccordionSummary, Typography, Menu, MenuItem } from '@mui/material';
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './NFTMintv2.css'
+import { Dropdown, DropdownButton, InputGroup } from 'react-bootstrap';
+import {FormatListBulletedIcon, StarBorder, BarChart, LockOpen, Warning} from '@mui/icons-material';
+
 
 const NFTMintv2 = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const traits = [
+        {
+            icon: <FormatListBulletedIcon />,
+            title: "Properties",
+            description: "Textual traits that show up as rectangles"
+        },
+        {
+            icon: <StarBorder />,
+            title: "Level",
+            description: "Numerical traits that show as a progress bar"
+        },
+        {
+            icon: <BarChart />,
+            title: "Stats",
+            description: "Numerical traits that just show as numbers"
+        },
+        {
+            icon: <LockOpen />,
+            title: "Unlockable Content",
+            description: "Include unlockable content that can only be revealed by the owner of the item."
+        },
+        {
+            icon: <Warning />,
+            title: "Explicit & Sensitive Content",
+            description: "Set this item as explicit and sensitive content"
+        }
+    ];
+
     return (
 
-        <div style={{ marginTop: '30px', marginLeft: '56px', marginRight: '50%' }}>
+        <div className='mintContainer'>
             <h1 style={{ marginBottom: '30px' }}> Create New Item</h1>
             <div style={{ fontSize: '10pt', color: '#5b5b5b' }}> <span style={{ color: 'red' }}>*</span> Required fields</div>
             <strong>Image, Video, Audio, or 3D Model </strong>
@@ -44,6 +89,46 @@ const NFTMintv2 = () => {
             <p style={{ fontSize: '10pt', color: '#5b5b5b', marginBottom: '12pt' }}>
                 This is the collection where your item will appear.
             </p>
+            <Accordion id='collection' onClick={handleClick}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <text style={{ color: 'GrayText' }}>Select Collection</text>
+                </AccordionSummary>
+            </Accordion>
+            {/* <Form.Select aria-label='Select your collection'>
+                <option>Select Your Collection</option>
+            </Form.Select> */}
+            {/* <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Dropdown Item 1</MenuItem>
+                <MenuItem onClick={handleClose}>Dropdown Item 2</MenuItem>
+                <MenuItem onClick={handleClose}>Dropdown Item 3</MenuItem>
+            </Menu> */}
+
+            {/* <InputGroup className="mb-3" id='collection2'>
+                <Form.Control aria-label="Text input with dropdown button" placeholder='Select collection'/>
+
+                <DropdownButton
+                     variant="outline"
+                    id="input-group-dropdown-2"
+                    align="end"
+                    
+                >
+                    <Dropdown.Item href="#">Action</Dropdown.Item>
+                    <Dropdown.Item href="#">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                </DropdownButton>
+            </InputGroup> */}
+
+
 
         </div>
 
